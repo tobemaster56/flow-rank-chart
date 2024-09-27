3ec49ddd42e4c091069cd6c458f4cceb28a08c4f
景联 <lian.jing@shuyun.com>
2024-09-28 00:15:27 +0800

    按需引入

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-import * as echarts from 'echarts';
+// 按需引入 ECharts 核心模块
+import * as echarts from 'echarts/core';
+// 引入柱状图图表，图表后缀都为 Chart
+import { ScatterChart, LineChart } from 'echarts/charts';
+// 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent
+} from 'echarts/components';
+// 标签自动布局、全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers';
+
 import { merge, set } from 'lodash-es';
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+  ScatterChart,
+  LineChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer
+]);
 
 var dom = document.getElementById('chart-container');
 var myChart = echarts.init(dom, null, {
@@ -217,7 +249,7 @@ const defaultData = [
 ]
 
 function formatMoney(money) {
-   return money
+  return money
 }
 
 function run({ data = defaultData, height = 500 }) {
