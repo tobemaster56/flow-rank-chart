d20042b375e3a4af25614d18e1463334a27359df
景联 <lian.jing@shuyun.com>
2024-09-28 10:17:18 +0800

    优化了一些逻辑，看起来更舒服

--- src/index.js
+++ src/index.js
@@ -16,16 +16,16 @@ function formatMoney(money) {
   return money
 }
 
-function run({ data = defaultData, height = 500 }) {
+function getOption(data = defaultData, height = 500) {
   const chartHeight = height;
   const maxY = 100;
   const maxHeight = chartHeight - maxY;
 
   function createData(initData = []) {
-    const list = initData?.map((item) => ({
+    const list = initData.map(item => ({
       ...item,
       total: item.list.reduce((pre, cur) => pre + cur.value, 0),
-      list: item.list?.sort((a, b) => a.value - b.value)
+      list: item.list.sort((a, b) => a.value - b.value)
     }));
     const legendData = [];
     const xAxisData = [];
@@ -33,86 +33,69 @@ function run({ data = defaultData, height = 500 }) {
     let max = 0;
 
     // 生成x轴、图例数据
-    for (const dateIndex in list) {
-      const item = list[dateIndex];
+    list.forEach((item, dateIndex) => {
       xAxisData.push(item.date);
-      if (dateIndex < list?.length - 1) {
-        new Array(3).fill(0).forEach((_, lineIndex) => {
-          xAxisData.push(`line-${lineIndex}`);
-        });
+      if (dateIndex < list.length - 1) {
+        xAxisData.push(...Array(3).fill().map((_, lineIndex) => `line-${lineIndex}`));
       }
       max = Math.max(max, item.total);
-      for (const index in item.list) {
-        const dataItem = item.list[index];
-        if (!legendData?.includes(dataItem.name)) {
+      item.list.forEach(dataItem => {
+        if (!legendData.includes(dataItem.name)) {
           legendData.push(dataItem.name);
         }
-      }
-    }
+      });
+    });
 
     // 根据图例生成数据
-    for (const index in list) {
-      const item = list[index];
-      for (const name of legendData) {
-        const dataItem = item?.list?.find((dataItem) => dataItem.name === name);
+    list.forEach((item, index) => {
+      legendData.forEach(name => {
+        const dataItem = item.list.find(dataItem => dataItem.name === name);
         set(seriesDataMap, `${name}.${index}`, dataItem?.value);
-      }
-    }
+      });
+    });
 
-    const result = { list, legendData, xAxisData, seriesDataMap, max };
-    // console.log('result', result);
-    return result;
+    return { list, legendData, xAxisData, seriesDataMap, max };
   }
 
   function createLineChart({ seriesData = [], initDataResult }) {
     const { list, max } = initDataResult;
     const spaceLineSeries = [];
     const lineSeries = [];
-    // console.log('seriesData', seriesData);
-    for (const seriesIndex in seriesData) {
-      const seriesItem = seriesData[seriesIndex];
+
+    seriesData.forEach((seriesItem, seriesIndex) => {
       const defaultLineSeries = {
         type: 'line',
         name: seriesItem.name,
         stack: `Line-${seriesIndex}`,
         smooth: 0.3,
-        lineStyle: {
-          width: 0,
-          opacity: 0
-        },
+        lineStyle: { width: 0, opacity: 0 },
         symbol: 'none',
         showSymbol: false,
         triggerLineEvent: true,
         silent: true,
         areaStyle: {},
-        emphasis: {
-          focus: 'series'
-        }
+        emphasis: { focus: 'series' }
       };
 
       spaceLineSeries.push({
         ...defaultLineSeries,
-        areaStyle: {
-          opacity: 0
-        },
-        data: getLineData(seriesItem?.data, seriesItem.name, true)
+        areaStyle: { opacity: 0 },
+        data: getLineData(seriesItem.data, seriesItem.name, true)
       });
 
       lineSeries.push({
         ...defaultLineSeries,
-        data: getLineData(seriesItem?.data, seriesItem.name)
+        data: getLineData(seriesItem.data, seriesItem.name)
       });
-    }
+    });
 
     function getLineData(data, name, isSpace = false) {
-      const result = data?.map((_, index) => {
+      return data.map((_, index) => {
         const dateIndex = Math.floor(index / 4);
         const lineIndex = index % 4;
-
-        const item = data?.[index] || {};
-        const lastItem = data?.[index - (4 - lineIndex)] || {};
-        const nextItem = data?.[index + (4 - lineIndex)] || {};
-
+        const item = data[index] || {};
+        const lastItem = data[index - (4 - lineIndex)] || {};
+        const nextItem = data[index + (4 - lineIndex)] || {};
         const offset = getOffset({ list, dateIndex, name, max });
         const nextOffset = getOffset({ list, dateIndex: dateIndex + 1, name, max });
         let spaceValue;
@@ -124,9 +107,7 @@ function run({ data = defaultData, height = 500 }) {
             break;
           case 1:
             spaceValue = offset;
-            if (!nextItem?.radioValue) {
-              value = undefined;
-            }
+            if (!nextItem?.radioValue) value = undefined;
             break;
           case 2:
             spaceValue = (nextOffset + offset) / 2;
@@ -135,23 +116,13 @@ function run({ data = defaultData, height = 500 }) {
           case 3:
             spaceValue = nextOffset;
             value = nextItem.radioValue - nextOffset;
-            if (!lastItem?.radioValue) {
-              value = undefined;
-            }
+            if (!lastItem?.radioValue) value = undefined;
             break;
         }
-        if (!lastItem?.radioValue && !nextItem?.radioValue) {
-          value = undefined;
-        }
-        // console.log(lineIndex, item, offset, nextOffset, spaceValue, value);
-        const newItem = {
-          ...item,
-          value: isSpace ? spaceValue : value
-        };
-        return newItem;
+        if (!lastItem?.radioValue && !nextItem?.radioValue) value = undefined;
+
+        return { ...item, value: isSpace ? spaceValue : value };
       });
-      // console.log('result', result);
-      return result;
     }
 
     return [...spaceLineSeries, ...lineSeries];
@@ -162,79 +133,69 @@ function run({ data = defaultData, height = 500 }) {
     const { list, legendData, xAxisData, seriesDataMap, max } = initDataResult;
     const seriesData = [];
 
-    for (const seriesIndex in Object.keys(seriesDataMap)) {
-      const name = Object.keys(seriesDataMap)[seriesIndex];
+    Object.keys(seriesDataMap).forEach(name => {
       const data = seriesDataMap[name];
       seriesData.push({
         name,
         type: 'scatter',
         symbol: 'rect',
         z: 3,
-        itemStyle: {
-          opacity: 1
-        },
+        itemStyle: { opacity: 1 },
         label: {
           show: true,
           color: '#fff',
-          formatter: (params) => formatMoney(params.data.realValue, 0)
+          formatter: params => formatMoney(params.data.realValue, 0)
         },
         tooltip: {
           trigger: 'item',
-          formatter: (params) => {
-            return `<div>
-            <div>年度月份：${params.name}</div>
-            <div>${params.seriesName}：${formatMoney(params.data.realValue, 0)}</div>
-          </div>`;
-          }
+          formatter: params => `
+            <div>
+              <div>年度月份：${params.name}</div>
+              <div>${params.seriesName}：${formatMoney(params.data.realValue, 0)}</div>
+            </div>`
         },
         data: getChartData({ data, name })
       });
-    }
+    });
 
     function getChartData({ data = [], name }) {
-      const dataResult = [];
-      data?.forEach((value, dateIndex) => {
+      return data.flatMap((value, dateIndex) => {
         const y = maxY * (value / max);
         const ySize = maxHeight * (y / maxY);
         const offset = getOffset({ list, dateIndex, name, max });
         const radioValue = y + offset > 100 ? 100 : y + offset;
 
-        dataResult.push({
+        const result = [{
           name,
           value: radioValue,
           radioValue,
           realValue: value,
           symbolOffset: [0, '50%'],
           symbolSize: [50, ySize]
-        });
+        }];
 
-        if (dateIndex < data?.length - 1) {
-          new Array(3).fill(0).forEach((_, lineIndex) => {
-            dataResult.push({
-              value: '',
-              radioValue,
-              realValue: value,
-              isLine: true,
-              lineIndex
-            });
-          });
+        if (dateIndex < data.length - 1) {
+          result.push(...Array(3).fill().map((_, lineIndex) => ({
+            value: '',
+            radioValue,
+            realValue: value,
+            isLine: true,
+            lineIndex
+          })));
         }
+
+        return result;
       });
-      return dataResult;
     }
 
     const lineSeries = createLineChart({ seriesData, initDataResult });
 
     return {
       option: {
-        legend: {
-          data: legendData
-        },
+        legend: { data: legendData },
         xAxis: {
           data: xAxisData,
-          axisTick: {
-            show: false
-          }
+          axisTick: { show: false }
         },
         series: [...seriesData, ...lineSeries]
       }
@@ -243,73 +204,31 @@ function run({ data = defaultData, height = 500 }) {
 
   function getOffset({ list, dateIndex, name, max }) {
     const dateData = list[dateIndex]?.list || [];
-    const itemIndex = dateData?.findIndex((item) => item.name === name);
-
-    let offset = 0;
-    for (let i = 0; i < itemIndex; i++) {
-      const itemValue = dateData[i].value;
-      offset += maxY * (itemValue / max);
-    }
-
-    return offset;
+    const itemIndex = dateData.findIndex(item => item.name === name);
+    return dateData.slice(0, itemIndex).reduce((offset, item) => offset + maxY * (item.value / max), 0);
   }
 
   const { option: newOption } = createOption(data);
 
-  return merge(
-    {
-      grid: {
-        top: 40,
-        left: 20,
-        right: 20,
-        bottom: 40,
-        containLabel: true
-      },
-      yAxis: {
-        show: false,
-        max: maxY
-      },
-      tooltip: {
-        // show: true,
-        // trigger: 'axis',
-        // axisPointer: {
-        //   type: 'none'
-        // },
-        // formatter: (params, ticket) => {
-        //   // console.log('params', params, ticket);
-        //   return '';
-        // }
-      },
-      // dataZoom: [
-      //   {
-      //     type: 'slider',
-      //     filterMode: 'weakFilter',
-      //     showDataShadow: false,
-      //     showDetail: false,
-      //     brushSelect: false,
-      //     height: 20,
-      //     bottom: 10,
-      //     startValue: 1,
-      //     endValue: 5,
-      //     xAxisIndex: 0,
-      //     start: 0,
-      //     end: 100
-      //   }
-      // ],
-      xAxis: {
-        type: 'category',
-        data: newOption.xAxis.data,
-        axisLabel: {
-          formatter: function (value) {
-            return value?.includes('line') ? '' : value;
-          }
-        }
-      }
+  return merge({
+    grid: {
+      top: 40,
+      left: 20,
+      right: 20,
+      bottom: 40,
+      containLabel: true
     },
-    newOption
-  );
-}
-
-function getOption(data, height) {
-  return run({ data, height });
+    yAxis: {
+      show: false,
+      max: maxY
+    },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: newOption.xAxis.data,
+      axisLabel: {
+        formatter: value => value.includes('line') ? '' : value
+      }
+    }
+  }, newOption);
 }
